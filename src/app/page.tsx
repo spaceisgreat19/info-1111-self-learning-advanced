8aae4b37ff697acf1ec08de308a51254aa1b94e3
DDDDD <dddddspaceisgreat@DDDDDs-MacBook-Pro.local>
2025-03-29 00:25:59 +1100

    Fixed JSX styling issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Link from 'next/link';
-import Image from 'next/image';
+import Link from 'next/link'; // Import Link from next/link
+import Image from 'next/image'; // Import Image for optimized images
 
 export default function Home() {
   return (
