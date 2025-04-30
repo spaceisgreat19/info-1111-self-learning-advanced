9a4bd11134b18fe6f28ba9e0f51a0da7afc2d1a5
DDDDD <dddddspaceisgreat@DDDDDs-MacBook-Pro.local>
2025-04-30 20:34:55 +1000

    fix header

--- components/Header.js
+++ components/Header.js
@@ -4,41 +4,35 @@ const Header = () => {
   return (
     <header style={{ background: '#333', color: '#fff', padding: '10px 0' }}>
       <nav>
-        <ul style={{ display: 'flex', justifyContent: 'center', listStyleType: 'none', margin: 0 }}>
+        <ul style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexWrap: 'wrap',
+          listStyleType: 'none',
+          padding: 0,
+          margin: 0,
+        }}>
           <li style={{ margin: '0 15px' }}>
-            <Link href="/" style={{ color: '#fff', textDecoration: 'none' }}>
-              Home
-            </Link>
+            <Link href="/" style={{ color: '#fff', textDecoration: 'none' }}>Home</Link>
           </li>
           <li style={{ margin: '0 15px' }}>
-            <Link href="/vote" style={{ color: '#fff', textDecoration: 'none' }}>
-              Vote
-            </Link>
+            <Link href="/vote" style={{ color: '#fff', textDecoration: 'none' }}>Vote</Link>
           </li>
           <li style={{ margin: '0 15px' }}>
-            <Link href="/finance" style={{ color: '#fff', textDecoration: 'none' }}>
-              Finance
-            </Link>
+            <Link href="/finance" style={{ color: '#fff', textDecoration: 'none' }}>Finance</Link>
           </li>
           <li style={{ margin: '0 15px' }}>
-            <Link href="/levy" style={{ color: '#fff', textDecoration: 'none' }}>
-              Levy
-            </Link>
+            <Link href="/levy" style={{ color: '#fff', textDecoration: 'none' }}>Levy</Link>
           </li>
           <li style={{ margin: '0 15px' }}>
-            <Link href="/maintenance" style={{ color: '#fff', textDecoration: 'none' }}>
-              Maintenance
-            </Link>
+            <Link href="/maintenance" style={{ color: '#fff', textDecoration: 'none' }}>Maintenance</Link>
           </li>
           <li style={{ margin: '0 15px' }}>
-            <Link href="/maintenance-requests" style={{ color: '#fff', textDecoration: 'none' }}>
-              Maintenance Requests
-            </Link>
+            <Link href="/maintenance-requests" style={{ color: '#fff', textDecoration: 'none' }}>Maintenance Requests</Link>
           </li>
-          <li style={{ margin: '0 15px', marginLeft: 'auto' }}>
-            <Link href="/login" style={{ color: '#fff', textDecoration: 'none' }}>
-              Login
-            </Link>
+          <li style={{ margin: '0 15px' }}>
+            <Link href="/login" style={{ color: '#fff', textDecoration: 'none' }}>Login</Link>
           </li>
         </ul>
       </nav>
