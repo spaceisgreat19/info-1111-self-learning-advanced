2397c429c71ae5a3a78b7cb1c4fa727596875bc5
DDDDD <dddddspaceisgreat@DDDDDs-MacBook-Pro.local>
2025-04-30 18:28:15 +1000

    fix vote GET handler

--- src/app/api/vote/route.ts
+++ src/app/api/vote/route.ts
@@ -10,23 +10,19 @@ export async function POST(request: NextRequest) {
   try {
     const { option, userId } = await request.json();
 
-    // Check if required fields are missing
     if (!option || !userId) {
       return NextResponse.json({ message: 'Missing required fields.' }, { status: 400 });
     }
 
-    // Insert the new vote into the database
     const result = await client.execute({
       sql: 'INSERT INTO votes (option, userId) VALUES (?, ?)',
       args: [option, userId],
     });
 
-    // Convert the BigInt to a string before returning it in the response
-    const id = result.lastInsertRowid.toString(); // Convert to string
+    const id = result.lastInsertRowid?.toString() || '';
 
     return NextResponse.json({ id, option, userId }, { status: 201 });
-  } catch (error) {
-    console.error('Error creating vote:', error);
+  } catch {
     return NextResponse.json({ message: 'Server error while creating vote.' }, { status: 500 });
   }
 }
